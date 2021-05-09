f765948e0fcb468a796264a4b44bdee6224bd4ed
Sosorini <maudlinsy@gmail.com>
2021-05-09 13:16:07 +0900

    IMPLEMENT pages/manager

--- store/actions/index.js
+++ store/actions/index.js
@@ -1,6 +1,13 @@
 import * as types from "./types";
 
-export const getUserInfo = () => (dispatch) =>
+export const getUserInfo = () => (dispatch) => {
   dispatch({
     type: types.GET_USER,
   });
+};
+
+export const scrapProducts = () => (dispatch) => {
+  dispatch({
+    type: types.SCRAP_PRODUCTS,
+  });
+};
