9af209e0576a18f0c1a2979071a469455891689a
Parksoyoon <maudlinsy@gmail.com>
2021-05-12 01:15:26 +0900

    REMOVE unused code

--- src/core/config/firebase.js
+++ src/core/config/firebase.js
@@ -1,23 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-// const {
-//   API_KEY,
-//   AUTH_DOMAIN,
-//   PJT_ID,
-//   STORAGE_BUCKET,
-//   MSG_SENDER_ID,
-//   APP_ID,
-// } = process.env;
-
-// const firebaseConfig = {
-//   apiKey: API_KEY,
-//   authDomain: AUTH_DOMAIN,
-//   projectId: PJT_ID,
-//   storageBucket: STORAGE_BUCKET,
-//   messagingSenderId: MSG_SENDER_ID,
-//   appId: APP_ID,
-// };
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
