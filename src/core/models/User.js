bdda62a996dba0aae3044d41623b002a340c0b60
Parksoyoon <maudlinsy@gmail.com>
2021-05-12 10:56:55 +0900

    TEMP

--- src/core/models/User.js
+++ src/core/models/User.js
@@ -2,18 +2,25 @@ import mongoose from "mongoose";
 import Adapters from "next-auth/adapters";
 
 export default class User extends Adapters.TypeORM.Models.User.model {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(name, email, image, emailVerified) {
+    super(name, email, image, emailVerified);
+  }
 }
 
 export const UserSchema = {
   // NOTE: name: "user"였던 부분을 extends해서 복사해주었다 소용없네..
-  ...Adapters.TypeORM.Models.User.schema,
+  // ...Adapters.TypeORM.Models.User.schema,
+  name: "User",
   target: User,
   columns: {
     ...Adapters.TypeORM.Models.User.schema.columns,
-    reviewId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Review",
-    },
+    reviews: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
     history: [String],
     recycleScore: {
       type: Number,
