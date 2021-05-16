282ef610992d8034a06a276ea7baf5684a234e87
Parksoyoon <maudlinsy@gmail.com>
2021-05-16 22:20:16 +0900

    FIX webLetter fetch to axios

--- src/pages/webLetter.js
+++ src/pages/webLetter.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import cheerio from "cheerio";
 import styled from "styled-components";
 
@@ -116,8 +117,7 @@ export default function WebLetter({ letters }) {
 }
 
 export async function getStaticProps() {
-  const res = await fetch("http://ecoseoul.or.kr/archives/category/%ec%9e%90%eb%a3%8c/webletter");
-  const html = await res.text();
+  const html = await axios.get("http://ecoseoul.or.kr/archives/category/%ec%9e%90%eb%a3%8c/webletter");
   const $ = cheerio.load(html.data);
   const $bodyList = $("ul.cat-list > li").children("a");
 
