0ecedc94dad7140af3874e0e735a93158fcb1239
Parksoyoon <maudlinsy@gmail.com>
2021-05-20 02:12:50 +0900

    FIX next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 module.exports = {
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: "empty",
-      };
-    }
-
-    return config;
+  images: {
+    domains: ["ecoseoul.or.kr"],
   },
   env: {
     managerAccount: "maudlinsy@gmail.com",
