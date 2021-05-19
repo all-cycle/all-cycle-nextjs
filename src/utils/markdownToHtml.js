596878471c506a10ae855ff31d83305bd68d4cf9
Parksoyoon <maudlinsy@gmail.com>
2021-05-20 00:48:08 +0900

    FIX till webLetter, babelrc, package.json

--- src/utils/markdownToHtml.js
+++
@@ -1,9 +0,0 @@
-import remark from "remark";
-import html from "remark-html";
-
-async function markdownToHtml(markdown) {
-  const result = await remark().use(html).process(markdown);
-  return result.toString();
-}
-
-export default markdownToHtml;
