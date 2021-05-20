fa9018fd24b0fee9f00a0147561344b02fd6e78c
Parksoyoon <maudlinsy@gmail.com>
2021-05-20 12:31:27 +0900

    ADD full url

--- src/utils/callVisionAPI.js
+++ src/utils/callVisionAPI.js
@@ -12,9 +12,19 @@ async function callVisionAPI(uri) {
   };
 
   try {
-    const data = await fetchData("POST", process.env.GOOGLE_VISION_API_URL, body);
+    // const data = await fetchData("POST", process.env.GOOGLE_VISION_API_URL, body);
     // console.log("1", data.responses[0].textAnnotations[0].description);
     // console.log("2", data.responses[0].fullTextAnnotation.text);
+    const response = await fetch(process.env.GOOGLE_VISION_API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(body),
+    });
+
+    const data = await response.json();
 
     if (!Object.entries(data.responses[0]).length) {
       return [];
